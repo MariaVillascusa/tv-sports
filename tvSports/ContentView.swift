4900ec1dfa994534712d2323f7feb939d5273430
Maria Villascusa <meri.villa14@gmail.com>
2024-05-28 00:03:22 +0200

    Initial Commit

---
+++ tvSports/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tvSports
+//
+//  Created by Maria  Villascusa on 28/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tvSports/tvSportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvSportsApp.swift
+//  tvSports
+//
+//  Created by Maria  Villascusa on 28/5/24.
+//
+
+import SwiftUI
+
+@main
+struct tvSportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
